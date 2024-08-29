0c524fc4cefc79aa45f3bddc0ade95d58effc60e
Huihyeon Park <agnea0829@gmail.com>
2024-08-29 11:13:19 +0900

    초기 커밋.

---
+++ CSAdv2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSAdv2
+{
+    internal class Program
+    {
+
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            //ToString() 구현
+            public override string ToString()
+            {
+                return string.Format("Name:{0}, Price:{1}", Name, Price);
+            }
+            static void Main(string[] args)
+        {
+                
+        }
+    }
+}
